6c4c812c7e077514cb2222a9819d71dbb0d25fec
EverStarck <everstarckone@protonmail.com>
2023-05-10 21:04:00 -0600

    chore: add absolute imports

--- babel.config.js
+++ babel.config.js
@@ -4,8 +4,16 @@ module.exports = function (api) {
     presets: ["babel-preset-expo"],
     plugins: [
       "@babel/plugin-proposal-export-namespace-from",
-      "react-native-reanimated/plugin",
       require.resolve("expo-router/babel"),
+      [
+        "module-resolver",
+        {
+          root: ["."],
+            alias: {
+            "@": "./",
+          },
+        },
+      ],
     ],
   };
 };
