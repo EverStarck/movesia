5e468794a56e3474d645b044d0500e5c09c63f45
Ever <everstarckone@protonmail.com>
2023-05-18 16:48:35 -0600

    refactor: update id dynamic page

--- app/movie/[id].tsx
+++ app/movie/[id].tsx
@@ -11,22 +11,24 @@ import { API_URL } from "@env";
 import { Stack, useRouter, useSearchParams } from "expo-router";
 
 import { Recommend } from "@/types/recommend";
-import { MovieParser } from "@/lib/movie";
 import tw from "@/lib/tw";
 import { MoviesContext } from "@/context/movies";
 import { Icons } from "@/components/icons";
+import { Poster } from "@/components/poster";
 
 const Movie = () => {
-  const { name } = useSearchParams();
+  const { id } = useSearchParams();
   const { movies, setMovies } = useContext(MoviesContext);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string>("");
 
+  console.log("🚀 ~ file: [id].tsx:21 ~ Movie ~ id:", id);
+
   async function fetchMovie() {
     // Check movie exists in context
-    console.log("here", name, movies);
+    console.log("here", id, movies);
     try {
-      const res = await fetch(`${API_URL}/recommend?movie=${name}`, {
+      const res = await fetch(`${API_URL}/recommend?movie=${id}`, {
         method: "GET",
         headers: {
           "Access-Control-Allow-Origin": "*",
@@ -36,33 +38,47 @@ const Movie = () => {
       console.log("🚀 ~ file: [name].tsx:31 ~ fetchMovie ~ res:", res);
 
       const data: Recommend = await res.json();
+      console.log("🚀 ~ file: [id].tsx:41 ~ fetchMovie ~ data:", data);
 
       if (data.error) {
         setError(data.message);
+        return;
       }
 
-      setMovies([...movies, data.data]);
-      console.log("🚀 ~ file: [name].tsx:34 ~ fetchMovie ~ data:", data);
-      console.log(MovieParser([...movies, data.data]), "xd");
+      setMovies({ ...movies, ...data.data });
+      setLoading(false);
+      setError("");
     } catch (error: any) {
+      console.log("🚀 ~ file: [name].tsx:49 ~ fetchMovie ~ error:", error);
       setError(error.message);
     }
-    setLoading(false);
   }
 
   useEffect(() => {
+    if (id === undefined) return;
     fetchMovie();
-  }, []);
+  }, [id]);
+
+  if (!id || typeof id !== "string") return;
+
+  if (error) {
+    console.log("🚀 ~ file: [id].tsx:58 ~ Movie ~ error:", error);
+    return (
+      <View style={tw`flex-1 bg-background justify-center items-center`}>
+        <Text style={tw`text-foreground`}>{error}</Text>
+      </View>
+    );
+  }
 
   return (
     <SafeAreaView
       style={tw.style(`flex-1`, {
-        paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+        // paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
       })}
     >
       <ScrollView
         showsVerticalScrollIndicator={false}
-        contentContainerStyle={tw`flex-1 bg-background px-8`}
+        contentContainerStyle={tw`flex-1 bg-background`}
       >
         <Stack.Screen options={{ headerShown: false }} />
 
@@ -75,19 +91,14 @@ const Movie = () => {
           </View>
         )}
 
-        {error && (
-          <View style={tw`flex-1 justify-center items-center`}>
-            <Text style={tw`text-foreground`}>{error}</Text>
-          </View>
+        {console.log("🚀 ~ file: [id].tsx:92 ~ Movie ~ loading:", loading)}
+        {console.log("🚀 ~ file: [id].tsx:98 ~ Movie ~ error:", error)}
+        {!loading && error === "" && (
+          <>
+            <Poster id={id} />
+            <Text style={tw`text-white`}>{id}</Text>
+          </>
         )}
-
-        <View style={tw`flex-1 justify-center items-center`}>
-          <Text style={tw`text-foreground`}>{name}</Text>
-        </View>
-
-        {/* <View style={tw`flex-1 pt-1`}>
-          <Text style={tw`text-foreground`}>My Details for: {id}</Text>
-        </View> */}
       </ScrollView>
     </SafeAreaView>
   );
