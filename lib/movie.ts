d0e5f02710f9a4f1941ff2c3df474975e8ab558e
Ever <everstarckone@protonmail.com>
2023-05-22 08:54:14 -0600

    feat: get recommended movies

--- lib/movie.ts
+++ lib/movie.ts
@@ -1,22 +1,26 @@
-import { Original, RecommendData } from "@/types/recommend";
+import { Recommend } from "@/types/recommend";
 
-export function MovieParser(state: RecommendData[]) {
-  return state.reduce(
-    (acc, cur) => {
-      if (cur.original.title in acc === false) {
-        acc[cur.original.title] = cur.original;
-      }
+// export function MovieParser(state: RecommendData[]) {
+//   return state.reduce(
+//     (acc, cur) => {
+//       if (cur.original.title in acc === false) {
+//         acc[cur.original.title] = cur.original;
+//       }
 
-      Object.keys(cur.recommended).forEach((rec) => {
-        if (rec in acc === false) {
-          acc[cur.recommended[rec].title] = cur.recommended[rec];
-        }
-      });
+//       Object.keys(cur.recommended).forEach((rec) => {
+//         if (rec in acc === false) {
+//           acc[cur.recommended[rec].title] = cur.recommended[rec];
+//         }
+//       });
 
-      return acc;
-    },
-    {} as {
-      [key: string]: Original;
-    }
-  );
+//       return acc;
+//     },
+//     {} as {
+//       [key: string]: Original;
+//     }
+//   );
+// }
+
+export function getRecomMoviesArr(movies: Recommend["data"], id: number) {
+  return movies[id].recommended.map((rec) => movies[rec]);
 }
